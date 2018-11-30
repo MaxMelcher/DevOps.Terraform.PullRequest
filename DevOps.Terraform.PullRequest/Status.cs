d99bf56e824cac022827823e52152a862a6862a0
Max Melcher <max.melcher@microsoft.com>
2018-11-30 21:22:42 +0100

    testing the docker container

---
+++ DevOps.Terraform.PullRequest/Status.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevOps.Terraform.PullRequest
+{
+    [Route("api/[controller]")]
+    public class Status : Controller
+    {
+        // GET
+        public IActionResult Hook()
+        {
+            return new StatusCodeResult(200);
+        }
+    }
+}
